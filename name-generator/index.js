ec7a22624be66c6376fc3409cfe9ff9c17fa776d
faith62 <phifeichemu@gmail.com>
2022-03-05 18:32:07 +0300

    use the parseInt() method to change the slice outputs back to numbers

--- name-generator/index.js
+++ name-generator/index.js
@@ -5,16 +5,19 @@ btn.addEventListener('click',(e) => {
 
     const date = document.getElementById("dateInput").value
     const newDate= date.toString()
-    let CC = newDate.slice(0,2);
-    let YY = newDate.slice(2,4);
-    let MM = newDate.slice(5,7);
-    let DD = newDate.slice(8,10);
+    let CC = parseInt(newDate.slice(0,2));
+    let YY = parseInt(newDate.slice(2,4));
+    let MM = parseInt(newDate.slice(5,7));
+    let DD = parseInt(newDate.slice(8,10));
+
+     let d =(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7
 
     console.log(date);
     console.log(CC);
     console.log(YY);
     console.log(MM);
-    console.log(DD);
+    console.log(d);
+
 
 })
 
