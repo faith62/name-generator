0a724e3c4cdcb915783431bb4246658f32a1e486
faith62 <phifeichemu@gmail.com>
2022-03-05 18:11:19 +0300

    using the DOM document.getElementById().value to retrieve date input

--- name-generator/index.js
+++ name-generator/index.js
@@ -1,9 +1,13 @@
-const btn= document.getElementById("submitButton");
+const btn = document.getElementById("submitButton")
 
-btn.addEventListener("click",(e) => {
+btn.addEventListener('click',(e) => {
     e.preventDefault()
 
     const date = document.getElementById("dateInput").value
-
+    // const newDate= date.toString()
+    // let CC = newDate.slice(0,2);
     console.log(date);
+    // console.log(CC);
+
 })
+
