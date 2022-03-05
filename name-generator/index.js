07952227dca238f86bcde76cc229c7ebbeaea5c5
faith62 <phifeichemu@gmail.com>
2022-03-05 18:26:13 +0300

    Use the slice() method to get century,year,month and day from the date input and use .toString method to first change

--- name-generator/index.js
+++ name-generator/index.js
@@ -4,10 +4,17 @@ btn.addEventListener('click',(e) => {
     e.preventDefault()
 
     const date = document.getElementById("dateInput").value
-    // const newDate= date.toString()
-    // let CC = newDate.slice(0,2);
+    const newDate= date.toString()
+    let CC = newDate.slice(0,2);
+    let YY = newDate.slice(2,4);
+    let MM = newDate.slice(5,7);
+    let DD = newDate.slice(8,10);
+
     console.log(date);
-    // console.log(CC);
+    console.log(CC);
+    console.log(YY);
+    console.log(MM);
+    console.log(DD);
 
 })
 
