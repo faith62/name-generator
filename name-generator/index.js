01f1c228122c645c29c2ac0ead28cdc25ebfbece
faith62 <phifeichemu@gmail.com>
2022-03-06 23:32:18 +0300

    Add getDay() method to return the day of the week

--- name-generator/index.js
+++ name-generator/index.js
@@ -1,32 +1,42 @@
-let maleNames=[Kwasi, Kwando, kwabena, Kwaku, Yaw, Kofi, Kwame];
-let femaleNames=[Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama]
+let maleNames=["Kwasi", "Kwando", "kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femaleNames=["Akosua", "Adwoa", "Abenaa"," Akua", "Yaa", "Afua", "Ama"]
 
 
 const btn = document.getElementById("submitButton")
 
-const male = document.getElementById("radioMale").checked
-const female = document.getElementById("radioFemale").checked
+
 
 btn.addEventListener('click',(e) => {
     e.preventDefault()
+    const male = document.getElementById("radioMale").checked
+    const female = document.getElementById("radioFemale").checked
+    let result = document.getElementById("result")
+     
+    const dateInput = document.getElementById("dateInput").value
+    let date= new Date(dateInput)
+     let dayOfTheWeek =date.getDay();
+    // const newDate= date.toString()
+    // let CC = parseInt(newDate.slice(0,2));
+    // let YY = parseInt(newDate.slice(2,4));
+    // let MM = parseInt(newDate.slice(5,7));
+    // let DD = parseInt(newDate.slice(8,10));
 
-    const date = document.getElementById("dateInput").value
-    const newDate= date.toString()
-    let CC = parseInt(newDate.slice(0,2));
-    let YY = parseInt(newDate.slice(2,4));
-    let MM = parseInt(newDate.slice(5,7));
-    let DD = parseInt(newDate.slice(8,10));
-
-     let dayOfTheWeek =(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7
-     let d= dayOfTheWeek.toFixed();
+    //  let dayOfTheWeek =(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7
+    //  let d= dayOfTheWeek.toFixed();
  
-     console.log(date);
-    console.log(CC);
-    console.log(YY);
-    console.log(MM);
-    console.log(d);
+    //  console.log(date);
+    // console.log(CC);
+    // console.log(YY);
+    // console.log(MM);
+    // console.log(d);
 
-    
+    if(male){
+       result.innerHTML= `Your Akan name is ${maleNames[dayOfTheWeek]}`;
+    }
+    else if(female)
+    {
+        result.innerHTML= `Your Akan name is ${femaleNames[dayOfTheWeek]}`;
+    }
 })
 
 
