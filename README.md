8dfab4380421b5adfb3b38a7636428c9ef7b82ba
faith62 <phifeichemu@gmail.com>
2022-03-07 11:46:05 +0300

    Add js,css, README files

---
+++ README.md
@@ -0,0 +1,38 @@
+# name-generator
+### Project description
+This is a website that takes the user's input as birthday date , then calculates the day of the week they were born and depending on their gender outputs one's Akan Name.Akan Names are derived from Ghanian culture. FRequently in Ghana, children are given their first name as a 'day name' which corresponds to the day in the week they were born.
+### By Faith Chemutai ,06/03/2022
+## Setup/instalation Requirements
+*You need to have a vs code/ atom installed
+*Access to Internet
+*A web browser
+*You need access to a computer, laptop, tablet or mobile phone.
+
+ 1.Clone the repository
+ git clone https://github.com/faith62/name-generator.git
+ 2.Open terminal command line then navigate to root folder of the application. `cd name-generator`
+ 3.Open `index.html`  on your Browser
+
+ ### Behaviour Driven Development
+ 1.Displays form For Entering Date and Gender
+   -INPUT: "User's Birthday Entered"
+   -INPUT: "User'S Gender Entered"
+   -OUTPUT: "Displays User's Akan Name According to the Date and Gender Submitted" 
+
+ ### Technology
+ HTML & CSS(Bootstrap)
+ JavaScript
+
+ ### Bugs
+ <p>Have you seen any bug? Contact me.</p>
+
+ ##### Link to LIve Site :[]
+
+ ## Authors Info
+Email: phifeichemu@gmail.com
+  
+ ### Licence
+ MIT Licence
+ copyright 2022 Chemutai
+
+
---
+++ index.js
@@ -0,0 +1,49 @@
+let maleNames=["Kwasi", "Kwando", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femaleNames=["Akosua", "Adwoa", "Abenaa"," Akua", "Yaa", "Afua", "Ama"]
+
+const btn = document.getElementById("submitButton")
+
+btn.addEventListener('click',(e) => {
+    e.preventDefault()
+    const male = document.getElementById("radioMale").checked
+    const female = document.getElementById("radioFemale").checked
+    let result = document.getElementById("result")
+     
+    const dateInput = document.getElementById("dateInput").value
+    let date= new Date(dateInput)
+    let dayOfTheWeek =date.getDay();
+    // const newDate= date.toString()
+    // let CC = parseInt(newDate.slice(0,2));
+    // let YY = parseInt(newDate.slice(2,4));
+    // let MM = parseInt(newDate.slice(5,7));
+    // let DD = parseInt(newDate.slice(8,10));
+
+    //  let dayOfTheWeek =(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7
+    //  let d= dayOfTheWeek.toFixed();
+ 
+    
+
+    if(male){
+       result.innerHTML= `Your Akan name is: ${maleNames[dayOfTheWeek]} .`;
+    }
+    else if(female){
+        result.innerHTML= `Your Akan name is: ${femaleNames[dayOfTheWeek]} .`;
+    }
+    else{
+        alert("You did not submit correct details! please select your birthdate and gender.")
+    }
+    
+    // let frm = document.getElementById("form");
+    // frm.reset();
+    
+})
+
+ function clearResult(){
+    let frm = document.getElementById("form");
+    frm.reset(); 
+
+    document.getElementById("result").innerHTML = " ";
+    alert("The form will be reset reset!")
+ }
+ 
+
